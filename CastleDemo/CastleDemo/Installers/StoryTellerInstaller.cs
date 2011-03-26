dc91f6230e5cfea120182c71b4f8dd4c47dd10c6
Jacqueline <cupcake@gmail.com>
2011-03-26 17:50:15 +0100

    Storyteller

---
+++ CastleDemo/CastleDemo/Installers/StoryTellerInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.MicroKernel.SubSystems.Configuration;
+using CastleDemo.Infrastructure;
+
+namespace CastleDemo.Installers
+{
+    public class StoryTellerInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+            Component.For<IStoryTeller>().ImplementedBy<DutchStoryTeller>());
+        }
+    }
+}
